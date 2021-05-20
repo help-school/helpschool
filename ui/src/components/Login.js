b6234464ccdc6885bbe7fe77cf8282f7a33bbcf9
Venkatesh Arjunan <venkatesha@venkateshs-air.lan>
2021-05-20 14:17:00 -0700

    login initial file

--- ui/src/components/Login.js
+++ ui/src/components/Login.js
@@ -12,46 +12,15 @@ function Login() {
 
   const [status, setStatus] = useState("");
 
-  const teacherNameRef = useRef(null)
-  const teacherPhoneRef = useRef(null)
-  const teacherEmailRef = useRef(null)
-  const productUrlRef = useRef(null)
-  const quantityRef = useRef(null)
-  const schoolNameRef = useRef(null)
-  const addressRef = useRef(null)
-  const districtRef = useRef(null)
-  const stateRef = useRef(null)
-  const pincodeRef = useRef(null)
-  const postTeachersRequest = useSelector(({schoolReducer}) => schoolReducer.postTeachersRequest);
-
-  // function handleChange(event) {
-  //   alert(event.target.name + ":" + event.target.value )
-  //   alert(teacherNameRef.current.value)
-  //   setTeacherName( event.target.value)
-  // }
-
+  
+  
   function handleSubmit(event){
      
       event.preventDefault();
-      const item = {
-        teacher_name: teacherNameRef.current.value,
-        teacher_email: teacherEmailRef.current.value,
-        teacher_phone: teacherPhoneRef.current.value,
-        url: productUrlRef.current.value,
-        quantity_needed: quantityRef.current.value,
-        address: schoolNameRef.current.value,
-        place: addressRef.current.value,
-        district: districtRef.current.value,
-        state: stateRef.current.value,
-        country: "India",
-        photo_link: "",
-        extra_info: "{}",
-        pincode: pincodeRef.current.value}
-     
-      console.log(item)  
-      dispatch(Actions.postTeacherRequest(item));
-      setStatus("success")
-      setStatus("failure")
+      //console.log(item)  
+      // dispatch(Actions.postTeacherRequest(item));
+      // setStatus("success")
+      // setStatus("failure")
 
       console.log("Submit clicked: ", event.target)
   }
